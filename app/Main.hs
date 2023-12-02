6cae5bf0e2d4f80960ce655c098e824c789afa69
uni <uniwuni@protonmail.com>
2023-12-02 10:50:15 +0100

    text refactor

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,9 @@
 
 module Main where
 import System.Environment
-
+import System.TimeIt
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Day1
 import Day2
 import THDays (days)
@@ -12,5 +14,5 @@ main = do
   [day_str, part_str, file] <- getArgs
   let day = (read day_str :: Int) - 1
   let part = (read part_str :: Int) - 1
-  contents <- readFile file
-  putStrLn $ ($(days 1) !! (2 * day + part)) contents
+  contents <- T.readFile file
+  timeIt $ putStrLn $ ($(days 1) !! (2 * day + part)) contents
--- lib/Day1.hs
+++ lib/Day1.hs
@@ -16,10 +16,10 @@ maxIndex xs = elemIndex (maximum xs) xs
 digits :: [T.Text]
 digits = ["one","two","three","four","five","six","seven","eight","nine"] ++ (T.pack <$> show <$> [1..9])
 
-parse :: String -> [String]
-parse = lines
+parse :: T.Text -> [T.Text]
+parse = T.lines
 
-extractFromLine' :: String -> Maybe Int
+extractFromLine' :: T.Text -> Maybe Int
 extractFromLine' s = do
   h' <- firstIndex
   l' <- lastIndex
@@ -27,27 +27,27 @@ extractFromLine' s = do
   let l = (l' `mod` 9) + 1
   pure $ 10 * h + l
   where
-        indices_first = fst <$> flip T.breakOn (T.pack s) <$> digits
-        indices_last = fst <$> flip T.breakOnEnd (T.pack s) <$> digits
+        indices_first = fst <$> flip T.breakOn s <$> digits
+        indices_last = fst <$> flip T.breakOnEnd s <$> digits
         firstIndex, lastIndex :: Maybe Int
         (first, firstIndex) = (minimum indices_first, minIndex indices_first)
         (last, lastIndex) = (maximum indices_last, maxIndex indices_last)
 
-extractFromLine :: String -> Maybe Int
+extractFromLine :: T.Text -> Maybe Int
 extractFromLine xs = do
-  h <- S.head ys
-  l <- S.last ys
+  (h,_) <- T.uncons ys
+  (_,l) <- T.unsnoc ys
   pure $ 10 * read [h] + read [l]
-  where ys = filter isNumber xs
+  where ys = T.filter isNumber xs
 
-extractTotal :: [String] -> Int
+extractTotal :: [T.Text] -> Int
 extractTotal = sum . catMaybes . map extractFromLine
 
-extractTotal' :: [String] -> Int
+extractTotal' :: [T.Text] -> Int
 extractTotal' = sum . catMaybes . map extractFromLine'
 
-day1part1 :: String -> String
+day1part1 :: T.Text -> String
 day1part1 = show . extractTotal . parse
 
-day1part2 :: String -> String
+day1part2 :: T.Text -> String
 day1part2 = show . extractTotal' . parse
